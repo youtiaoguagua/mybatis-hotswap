bc931c17998799ff517b3725f0bfb01d3864e90f
youtiaoguagua <2049150434@qq.com>
2021-12-18 00:39:59 +0800

    format

--- mybatis-reload-web/src/router/index.js
+++ mybatis-reload-web/src/router/index.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+Vue.use(Router);
 
 /* Layout */
-import Layout from '@/layout'
+import Layout from "@/layout";
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -32,75 +32,78 @@ import Layout from '@/layout'
  */
 export const constantRoutes = [
   {
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true
+    path: "/login",
+    component: () => import("@/views/login/index"),
+    hidden: true,
   },
 
   {
-    path: '/404',
-    component: () => import('@/views/404'),
-    hidden: true
+    path: "/404",
+    component: () => import("@/views/404"),
+    hidden: true,
   },
 
   {
-    path: '/',
+    path: "/",
     component: Layout,
-    redirect: '/dashboard',
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: { title: '控制台', icon: 'dashboard' }
-    }]
+    redirect: "/dashboard",
+    children: [
+      {
+        path: "dashboard",
+        name: "Dashboard",
+        component: () => import("@/views/dashboard/index"),
+        meta: { title: "控制台", icon: "dashboard" },
+      },
+    ],
   },
 
   {
-    path: '/form',
+    path: "/form",
     component: Layout,
     children: [
       {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'XML目录', icon: 'form' }
+        path: "index",
+        name: "Form",
+        component: () => import("@/views/form/index"),
+        meta: { title: "XML目录", icon: "form" },
       },
       {
-        path: 'edit/:id',
-        component: () => import('@/views/form/edit'),
-        name: 'EditXml',
-        meta: { title: '修改XML', noCache: true, activeMenu: '/form/index' },
-        hidden: true
-      }
-    ]
+        path: "edit/:id",
+        component: () => import("@/views/form/edit"),
+        name: "EditXml",
+        meta: { title: "修改XML", noCache: true, activeMenu: "/form/index" },
+        hidden: true,
+      },
+    ],
   },
   {
-    path: 'external-link',
+    path: "external-link",
     component: Layout,
     children: [
       {
-        path: 'https://github.com/youtiaoguagua',
-        meta: { title: '关于', icon: 'link' }
-      }
-    ]
+        path: "https://github.com/youtiaoguagua",
+        meta: { title: "关于", icon: "link" },
+      },
+    ],
   },
 
   // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true }
-]
+  { path: "*", redirect: "/404", hidden: true },
+];
 
-const createRouter = () => new Router({
-  // mode: 'history', // require service support
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
-})
+const createRouter = () =>
+  new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes,
+  });
 
-const router = createRouter()
+const router = createRouter();
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+  const newRouter = createRouter();
+  router.matcher = newRouter.matcher; // reset router
 }
 
-export default router
+export default router;
--- mybatis-reload-web/src/views/form/index.vue
+++ mybatis-reload-web/src/views/form/index.vue
@@ -1,64 +1,60 @@
 <template>
   <div class="app-container">
     <el-table
-    v-loading="listLoading"
-    :data="list.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
-    style="width: 100%">
-    <el-table-column
-      label="mybatis文件"
-      prop="name">
-    </el-table-column>
-    <el-table-column
-      label="路径"
-      prop="path">
-    </el-table-column>
-    <el-table-column
-      align="right">
-      <template slot="header" slot-scope="scope">
-        <el-input
-          v-model="search"
-          size="mini"
-          placeholder="输入关键字搜索"/>
-      </template>
-      <template slot-scope="scope">
-        <router-link :to="'/form/edit/'+scope.row.name">
+      v-loading="listLoading"
+      :data="
+        list.filter(
+          (data) =>
+            !search || data.name.toLowerCase().includes(search.toLowerCase())
+        )
+      "
+      style="width: 100%"
+    >
+      <el-table-column label="mybatis文件" prop="name"> </el-table-column>
+      <el-table-column label="路径" prop="path"> </el-table-column>
+      <el-table-column align="right">
+        <template slot="header" slot-scope="scope">
+          <el-input v-model="search" size="mini" placeholder="输入关键字搜索" />
+        </template>
+        <template slot-scope="scope">
+          <router-link :to="'/form/edit/' + scope.row.name">
             <el-button type="primary" size="small" icon="el-icon-edit">
               编辑
             </el-button>
           </router-link>
-      </template>
-    </el-table-column>
-  </el-table>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-import { getList } from '@/api/table'
-  export default {
-    data() {
-      return {
-        listLoading:false,
-        list: [],
-        search: ''
-      }
-    },
-      created() {
-    this.getXmlData()
+import { getList } from "@/api/table";
+export default {
+  data() {
+    return {
+      listLoading: false,
+      list: [],
+      search: "",
+    };
   },
-    methods: {
-      getXmlData(){
-      this.listLoading = true
-      getList().then(response => {
-        this.list = response.data
-        this.listLoading = false
-      })
-      }
+  created() {
+    this.getXmlData();
+  },
+  methods: {
+    getXmlData() {
+      this.listLoading = true;
+      getList().then((response) => {
+        this.list = response.data;
+        this.listLoading = false;
+      });
     },
-  }
+  },
+};
 </script>
 
 <style scoped>
-.line{
+.line {
   text-align: center;
 }
 </style>
